d630f58061250a3285e73fba5d74b4bd341e9c6c
marcusPrado02 <silvamarcusprado@gmail.com>
2022-05-03 19:59:04 -0300

    Update Models

--- src/src/main/java/model/Document.java
+++ src/src/main/java/model/Document.java
@@ -1,5 +1,60 @@
 package model;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+
 public class Document {
+   protected int ID;
+   protected int UserID;
+   protected String DocName;
+   protected LocalDateTime DateCreation;
+
+   
+   public Document(){
+     ID = -1;
+     UserID = -1;
+     DocName = "";
+     DataCreation = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+
+   }
+
+
+   public Document(int ID,int UserID,String DocName,LocalDateTime DateCreation){
+
+     setID(ID);
+     setUserID(UserID);
+     setDocName(DocName);
+     setDateCreation(DateCreation);
+
+   }
+
+
+
+   public  int getID(){   return this.ID;   }
+   public  int getUserID(){   return this.UserID;   }
+   public  String getDocName(){   return this.DocName;   }
+   public  LocalDateTime getDateCreation(){    return this.DateCreation  } 
+
+
+
+   public static void setID(int ID){   this.ID = ID;    }
+   public static void setUserID(int UserID){   this.UserID = UserID;   }
+   public static void setDocName(String DocName){   this.DocName = DocName;   }
+   public static void setDateCreation(LocalDateTime DateCreation){  this.DateCreation = DateCreation;  }
+
+
+   @Override
+    public String toString() {
+        return "Document ID: " + this.ID + "   User ID: " + this.UserID + "   Document Name: " + this.DocName + "   Date of Creation: "
+            + this.DateCreation;
+    }
     
+
+    @Override
+      public boolean equals(Object obj) {
+          return (this.getID() == ((Document) obj).getID());
+    } 
+
 }
--- src/src/main/java/model/User.java
+++ src/src/main/java/model/User.java
@@ -1,5 +1,57 @@
 package model;
 
 public class User {
+    protected int ID;
+    protected String Name;
+    protected String Email;
+    protected String RecoveryEmail;
+    protected String UserPassword;
+
+
+    public Produto() {
+        ID = -1;
+        Name = "";
+        Email = "";
+        RecoveryEmail = "";
+        UserPassword = "";
+    }
+
+
+    public Produto(int ID, String Name, String Email, String RecoveryEmail, String UserPassword) {
+          setID(ID);
+          setName(Name);
+          setEmail(Email);
+          setRecoveryEmail(RecoveryEmail);
+          setUserPassword(UserPassword);
     
+    } 
+
+
+    public int getID() {     return this.ID;    }
+    public String getName() {   return this.Name;    }
+    public String getEmail() {     return this.Email;     }
+    public String getRecoveryEmail() {  return this.RecoveryEmail;   }
+    public String getUserPassword() {     return this.UserPassword;    }
+    
+    
+    
+    public void setID(int ID) {    this.ID = ID;    }
+    public void setName(String Name) {   this.Name = Name;    }
+    public void setEmail(String Email) {   this.Email = Email;    }
+    public void setRecoveryEmail(String RecoveryEmail) {   this.RecoveryEmail  = RecoveryEmail;   }
+    public void setUserPassword(String UserPassword) {    this.UserPassword = UserPassword;    }
+
+
+    @Override
+    public String toString() {
+          return "User ID: " + this.ID + "   UserName: " + this.Name + "   Email: " + this.email + "   Recovery Email: "
+              + this.RecoveryEmail  + "   Password: " + this.UserPasssword;
+    }
+   
+
+    @Override
+    public boolean equals(Object obj) {
+            return (this.getID() == ((User) obj).getID());
+    } 
+
 }
