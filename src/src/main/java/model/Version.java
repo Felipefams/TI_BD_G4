679ac7efef66c28b5d42241b9d8dcac94e6d8f42
Felipefams <augustofams@gmail.com>
2022-05-04 15:56:58 -0300

    fix privates names

--- src/src/main/java/model/Version.java
+++ src/src/main/java/model/Version.java
@@ -6,21 +6,21 @@ import java.time.temporal.ChronoUnit;
 public class Version {
     private int versionid;
     private int documentID;
-    private LocalDateTime DateOfCreation;
-    private String LinkOfAccess;
+    private LocalDateTime CreationDate;
+    private String AccessLink;
 
     public Version() {
         versionid = -1;
         documentID = -1;
-        DateOfCreation = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
-        LinkOfAccess = "";
+        CreationDate = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+        AccessLink = "";
     }
 
-    public Version(int versionid, int documentID, LocalDateTime DateOfCreation, String LinkOfAccess) {
+    public Version(int versionid, int documentID, LocalDateTime CreationDate, String AccessLink) {
         setVersionID(versionid);
         setdocumentID(documentID);
-        setData_of_Creation(DateOfCreation);
-        setLinkOfAccess(LinkOfAccess);
+        setData_of_Creation(CreationDate);
+        setAccessLink(AccessLink);
     }
 
     public int getVersionID() {
@@ -32,11 +32,11 @@ public class Version {
     }
 
     public LocalDateTime getData_of_Creation() {
-        return this.DateOfCreation;
+        return this.CreationDate;
     }
 
-    public String getLinkOfAccess() {
-        return this.LinkOfAccess;
+    public String getAccessLink() {
+        return this.AccessLink;
     }
 
     public void setVersionID(int versionid) {
@@ -47,18 +47,18 @@ public class Version {
         this.documentID = documentID;
     }
 
-    public void setData_of_Creation(LocalDateTime DateOfCreation) {
-        this.DateOfCreation = DateOfCreation;
+    public void setData_of_Creation(LocalDateTime CreationDate) {
+        this.CreationDate = CreationDate;
     }
 
-    public void setLinkOfAccess(String LinkOfAccess) {
-        this.LinkOfAccess = LinkOfAccess;
+    public void setAccessLink(String AccessLink) {
+        this.AccessLink = AccessLink;
     }
 
     @Override
     public String toString() {
         return "ID da Versão: " + this.versionid + "   ID do Documento: " + this.documentID + "   Data de Criação: "
-                + this.DateOfCreation;
+                + this.CreationDate;
     }
 
     @Override
