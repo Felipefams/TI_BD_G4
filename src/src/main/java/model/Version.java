d1b8c6144f606ca33bf9df8f8505cb79d7c0ed9a
Felipefams <augustofams@gmail.com>
2022-05-04 15:46:26 -0300

    fix attribute names
    indent fix

--- src/src/main/java/model/Version.java
+++ src/src/main/java/model/Version.java
@@ -4,45 +4,65 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
 public class Version {
-    private int id;
-    private int DocumentId;
+    private int versionid;
+    private int documentID;
     private LocalDateTime DateOfCreation;
     private String LinkOfAccess;
 
     public Version() {
-       id = -1;
-       DocumentId = -1;
-       DateOfCreation = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
-       LinkOfAccess = "";
+        versionid = -1;
+        documentID = -1;
+        DateOfCreation = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+        LinkOfAccess = "";
     }
 
-    public Version(int id,int DocumentId,LocalDateTime DateOfCreation,String LinkOfAccess) {
-        setId(id);
-        setDocumentId(DocumentId);
+    public Version(int versionid, int documentID, LocalDateTime DateOfCreation, String LinkOfAccess) {
+        setVersionID(versionid);
+        setdocumentID(documentID);
         setData_of_Creation(DateOfCreation);
         setLinkOfAccess(LinkOfAccess);
-     }
+    }
 
+    public int getVersionID() {
+        return this.versionid;
+    }
 
-    public int getId(){ return this.id; }
-    public int getDocumentId(){ return this.DocumentId; }
-    public LocalDateTime getData_of_Creation(){ return this.DateOfCreation; }
-    public String getLinkOfAccess(){ return this.LinkOfAccess; }
+    public int getdocumentID() {
+        return this.documentID;
+    }
 
+    public LocalDateTime getData_of_Creation() {
+        return this.DateOfCreation;
+    }
 
-    public void setId(int id){ this.id = id; }
-    public void setDocumentId(int DocumentId){ this.DocumentId = DocumentId; }
-    public void setData_of_Creation(LocalDateTime DateOfCreation){ this.DateOfCreation = DateOfCreation; }
-    public void setLinkOfAccess(String LinkOfAccess){ this.LinkOfAccess = LinkOfAccess; }
-    
+    public String getLinkOfAccess() {
+        return this.LinkOfAccess;
+    }
+
+    public void setVersionID(int versionid) {
+        this.versionid = versionid;
+    }
+
+    public void setdocumentID(int documentID) {
+        this.documentID = documentID;
+    }
+
+    public void setData_of_Creation(LocalDateTime DateOfCreation) {
+        this.DateOfCreation = DateOfCreation;
+    }
+
+    public void setLinkOfAccess(String LinkOfAccess) {
+        this.LinkOfAccess = LinkOfAccess;
+    }
 
     @Override
-	public String toString() {
-		return "ID da Versão: " + this.id + "   ID do Documento: " + this.DocumentId + "   Data de Criação: " + this.DateOfCreation ;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		return (this.getId() == ((Version) obj).getId());
-	}	
+    public String toString() {
+        return "ID da Versão: " + this.versionid + "   ID do Documento: " + this.documentID + "   Data de Criação: "
+                + this.DateOfCreation;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return (this.getVersionID() == ((Version) obj).getVersionID());
+    }
 }
